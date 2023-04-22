7c450e1ebcdfd5e2ee150e722682c7f762c168e0
Matt Enyeart <menyeart1@gmail.com>
2023-04-22 14:48:31 -0600

    feat: add error handling to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from NoMethodError::undef_method, with: :render_bad_location_response
+
+  def render_invalid_response(exception)
+    render json: ErrorSerializer.new(exception.message).invalid_request, status: :not_found
+  end
+
+  def render_bad_location_response
+    render json: ErrorSerializer.new("Invalid location params").invalid_request, status: :not_found
+  end
 end
