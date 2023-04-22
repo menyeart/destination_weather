b1ba526abe9c8fa304db6fddbac8aebce199c30b
Matt Enyeart <menyeart1@gmail.com>
2023-04-22 14:51:36 -0600

    feat: create weather forecast poro

---
+++ app/poros/weather_forecast.rb
@@ -0,0 +1,10 @@
+class WeatherForecast
+  attr_reader :current_weather, :daily_weather, :hourly_weather, :id
+
+  def initialize(current, daily, hourly)
+    @id = nil
+    @current_weather = current
+    @daily_weather = daily
+    @hourly_weather = hourly
+  end
+end
