cca70064038d8362b43a82ab98a73677564dc578
Matt Enyeart <menyeart1@gmail.com>
2023-04-22 18:24:55 -0600

    test:create spec for api key module

---
+++ spec/features/api_key_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'API Key' do
+  describe 'creation' do
+    it 'will create an api key' do
+      key = ApiKey.generator
+      
+      expect(key).to be_kind_of(String)
+    end
+  end
+end
