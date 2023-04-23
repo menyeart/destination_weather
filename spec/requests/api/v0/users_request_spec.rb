79cbb688bd0fe617072a91dff4132dcd07d1afad
Matt Enyeart <menyeart1@gmail.com>
2023-04-22 18:25:24 -0600

    test: create user request spec

---
+++ spec/requests/api/v0/users_request_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe 'Users request spec', type: :request do
+  describe '#create' do
+    it 'adds a user to the database, generates an api key and returns a successful response with id, email and api key' do
+      data_keys = [ :id, :type, :attributes]
+      attribute_keys = [:email, :api_key]
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      body =  {
+        
+        'email': 'whatever@example.com',
+        'password': 'password',
+        'password_confirmation': 'password'
+                                               
+              }
+      post "/api/v0/users", headers: headers, params: JSON.generate(body)
+
+      user = JSON.parse(response.body, symbolize_names: true)   
+
+      expect(response).to be_successful
+      expect(user).to have_key(:data)
+      expect(user[:data].keys).to eq(data_keys)
+      expect(user[:data][:attributes].keys).to eq(attribute_keys)
+    end
+
+    it "returns an error response if the passwords dont match" do
+      error_keys = [:status, :title, :detail]
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      body =  {
+        
+        'email': 'whatever@example.com',
+        'password': 'password',
+        'password_confirmation': 'passwor'
+                                               
+              }
+      post "/api/v0/users", headers: headers, params: JSON.generate(body)
+      error = JSON.parse(response.body, symbolize_names: true)   
+
+      expect(response).to_not be_successful
+      expect(error).to have_key(:errors)
+      expect(error[:errors].first.keys).to eq(error_keys)
+      expect(error[:errors].first[:detail]).to eq(["Invalid input"])
+    end
+
+    it "returns an error response if the email is already in the dtabase" do
+      error_keys = [:status, :title, :detail]
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      body =  {
+        
+        'email': 'whatever@example.com',
+        'password': 'password',
+        'password_confirmation': 'password'
+                                               
+              }
+      post "/api/v0/users", headers: headers, params: JSON.generate(body)
+      post "/api/v0/users", headers: headers, params: JSON.generate(body)
+
+      error = JSON.parse(response.body, symbolize_names: true)   
+  
+      expect(response).to_not be_successful
+      expect(error).to have_key(:errors)
+      expect(error[:errors].first.keys).to eq(error_keys)
+      expect(error[:errors].first[:detail]).to eq(["Validation failed: Email has already been taken"])
+    end
+  end
+end
