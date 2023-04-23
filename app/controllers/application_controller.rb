e9256d2cbbef7b76b069c5d06043d802e1cce553
Matt Enyeart <menyeart1@gmail.com>
2023-04-22 18:09:27 -0600

    feat:reafactor resuce methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::API
-  rescue_from NoMethodError::undef_method, with: :render_bad_location_response
+  rescue_from NoMethodError::undef_method, with: :render_bad_input_response
+  rescue_from JSON::ParserError, with: :render_invalid_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
   def render_invalid_response(exception)
     render json: ErrorSerializer.new(exception.message).invalid_request, status: :not_found
   end
 
-  def render_bad_location_response
-    render json: ErrorSerializer.new("Invalid location params").invalid_request, status: :not_found
+  def render_bad_input_response
+    render json: ErrorSerializer.new("Invalid input").invalid_request, status: :not_found
   end
 end
