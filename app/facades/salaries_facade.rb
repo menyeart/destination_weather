1c070b749aa877235f4b590ace58321fc2ee6ccb
Matt Enyeart <menyeart1@gmail.com>
2023-04-24 11:02:29 -0600

    Add additional logic to salaries facade

--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -2,6 +2,7 @@ class SalariesFacade
 
   def initialize(location)
     @location = location
+    @location_service ||= LocationService.new(location).convert_location
     @salaries ||= TeleportService.new(location).get_salary_info
     @current_weather ||= WeatherService.new(extract_location).current_weather
   end
@@ -20,7 +21,7 @@ class SalariesFacade
   end
 
   def extract_location
-    location =  @location[:results].first[:locations].first[:latLng]
+    location =  @location_service[:results].first[:locations].first[:latLng]
     loc_string = "#{location[:lat]}, #{location[:lng]}"
     loc_string
   end
@@ -28,8 +29,9 @@ class SalariesFacade
   def get_weather_forecast
     weather = Hash.new(nil)
     binding.pry
-    weather[:summary] = @current_weather
-    weather[:temperature] = @current_weather
+    weather[:summary] = @current_weather[:current][:condition][:text]
+    weather[:temperature] = @current_weather[:current][:temp_f]
+    weather
   end
 
   def create_salaries_object
