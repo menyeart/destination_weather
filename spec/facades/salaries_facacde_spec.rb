66ab532f1524ffb145c24e20a87c6b072daa8c2c
Matt Enyeart <menyeart1@gmail.com>
2023-04-24 11:46:21 -0600

    Add additional tests, refactor to account for new salary parse method

--- spec/facades/salaries_facacde_spec.rb
+++ spec/facades/salaries_facacde_spec.rb
@@ -10,9 +10,46 @@ RSpec.describe SalariesFacade do
     end
 
     it "parses salary info for the selected jobs" do
-      salaries = SalariesFacade.new("Denver").parse_salaries
+      salaries = SalariesFacade.new("Denver").parse_salaries_refactor
+      keys = ["title", "min", "max"]
+
+      expect(salaries).to be_a(Array)
+        salaries.each do |salary|
+          expect(salary.keys).to eq(keys)
+          expect(salary["title"]).to be_a(String)
+          expect(salary["min"]).to be_a(Float)
+          expect(salary["max"]).to be_a(Float)
+        end
+    end
+
+    it "returns a lat/long location from the input city" do
+      location = SalariesFacade.new("Denver").extract_location
+      
+      expect(location).to be_a(String)
+      expect(location).to_not eq("Denver")
+    end
+
+    it "parses weather data and returns a basic forecast" do
+      forecast = SalariesFacade.new("Denver").get_weather_forecast
+ 
+      expect(forecast).to be_a(Hash)
+      expect(forecast).to have_key(:summary)
+      expect(forecast[:summary]).to be_a(String)
+      expect(forecast).to have_key(:temperature)
+      expect(forecast[:temperature]).to be_a(Float)
+    end
+
+    it "creates a salaries object" do
+      forecast = SalariesFacade.new("Denver").create_salaries_object
+
       binding.pry
-      expect(trip).to be_a(RoadTrip)
+      expect(forecast.destination).to eq("Denver")
+      expect(forecast.forecast).to be_a(Hash)
+      expect(forecast.forecast.keys).to eq([:summary, :temperature])
+      expect(forecast.salaries).to be_a(Array)
+      forecast.salaries.each do |job|
+        expect(job.keys).to eq(["title", "min", "max"])
+      end
     end
   end
 end
