baad9f284f6a9b25ea7bddc7f2ce0b317b30bfb9
Matt Enyeart <menyeart1@gmail.com>
2023-04-24 14:54:46 -0600

    refactor: change user model encrypt api key to determnisitc

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_create :set_api_key
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
-  encrypts :api_key
+  encrypts :api_key, deterministic: true, downcase: true
 
   has_secure_password
 
