07830599d54aa50ee91dd5a3a76fe1045248934c
Matt Enyeart <menyeart1@gmail.com>
2023-04-24 11:15:57 -0600

    Add additional expets to salaries request spec

--- spec/requests/api/v1/salaries_request_spec.rb
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 RSpec.describe 'Salaries request spec', type: :request do
   describe '#seach', :vcr do
     it 'returns a salary object if inputs are valid' do
+      data_keys = [:id, :type, :attributes]
+      attribute_keys = [:destination, :forecast, :salaries]
+      salary_keys = [:title, :min, :max]
       headers = { 'CONTENT_TYPE' => 'application/json' }
-
       get "/api/v1/salaries?destination=denver", headers: headers, params: JSON.generate(body)
       
-      user = JSON.parse(response.body, symbolize_names: true)   
-
-      # expect(response).to be_successful
-      # expect(response.status).to eq(200)
-      # expect(user).to have_key(:data)
-      # expect(user[:data].keys).to eq(data_keys)
-      # expect(user[:data][:attributes].keys).to eq(attribute_keys)
+      salaries = JSON.parse(response.body, symbolize_names: true)   
+     
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(salaries).to have_key(:data)
+      expect(salaries[:data].keys).to eq(data_keys)
+      expect(salaries[:data][:attributes].keys).to eq(attribute_keys)
+      expect(salaries[:data][:attributes][:salaries]).to be_a(Array)
+      expect(salaries[:data][:attributes][:salaries].count).to eq(7)
+      salaries[:data][:attributes][:salaries].each do |salary|
+        expect(salary.keys).to eq(salary_keys)
+      end
     end
   end
 end
